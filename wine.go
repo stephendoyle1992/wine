3179e6981ca5f99254f1db8d10311f724c600963
Jordanhewko <hewkoj2@mymacewan.ca>
2019-01-19 17:08:34 -0700

    Add Region grabber

--- wine.go
+++ wine.go
@@ -3,7 +3,7 @@ package main
 import (
 	"encoding/json"
 	"net/http"
-
+	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jmoiron/sqlx"
@@ -37,17 +37,53 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/api/countries/", getCountryList).Methods("GET")
+	r.HandleFunc("/api/variety/", getVarietyList).Methods("GET")
+	r.HandleFunc("/api/{countries}/region1/", getRegion1).Methods("GET")
 
-	http.ListenAndServe("8888", r)
+	err = http.ListenAndServe(":8888", r)
+	fmt.Println(err)
+}
+func getVarietyList(w http.ResponseWriter, r *http.Request) {
+	q := "select distinct variety from Wine"
+	
+	varieties := []Wine{}
+
+	if err := Db.Select(&varieties, q); err != nil {
+		fmt.Println(err)
+		
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	encoder := json.NewEncoder(w)
+	encoder.Encode(varieties)
 
 }
+func getRegion1(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	countries := vars["countries"]
+	fmt.Println(countries)
+	regions := []Wine{}
+	q := `SELECT distinct region1 FROM Wine WHERE country=?`
+	
+	if err := Db.Select(&regions, q, countries); err != nil {
+		fmt.Println(err)
+		
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	encoder := json.NewEncoder(w)
+	encoder.Encode(regions)
+}
 func getCountryList(w http.ResponseWriter, r *http.Request) {
-	q := `PUT THE DB CALL IN HERE`
-
+	q := "select distinct country from Wine"
+	
 	countries := []Wine{}
 
 	if err := Db.Select(&countries, q); err != nil {
+		fmt.Println(err)
+		
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
